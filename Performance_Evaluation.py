78f650142467faf2cf7d6c6a4aa5fe3e19575d90
Hieu Le <lthieu2005@gmail.com>
2019-07-11 22:35:40 +0100

    Update_Performance
    Update_Performance function

--- Performance_Evaluation.py
+++ Performance_Evaluation.py
@@ -27,6 +27,12 @@ def categorise_mistake(Remove,Insert):
             print r,i
 
 good_vocab = []
+learnt_vocab = []
+false_vocab = []
+Improve_words = []
+Improve_point = 0
+Total_Unique_Words = 0
+stat = []
 for esl,filename in zip(esls,filenames):   
     #orginial_file_name = 'FileTest/ESL' + ' ' + str(esl_index) + ' ' + 'WELE' + '.docx'
     #compared_file_name = 'FileTest/ESL' + ' ' + str(esl_index) + ' ' + member_name + '.docx'
@@ -47,13 +53,42 @@ for esl,filename in zip(esls,filenames):
 
     print(compared_file_name)
     Remove, Insert = dmef.Detect_Mistakes_Each_File(original_content, compared_content)
-    Corlis = [x for x in original_content.split() if x not in Remove]
+    Corlis_0 = [x for x in original_content.split() if x not in Remove]
+    Corlis = [x for x in Corlis_0 if x in compared_content.split()]
+    Learn = [x for x in Corlis if x not in learnt_vocab]
+#    df_Learn = pd.DataFrame(Learn,columns=['word'])
+    df_Learn = pd.DataFrame([[x,Learn.count(x)] for x in Learn],columns=['word','freq'])
+#    Unique_Learn = df_Learn['word'].value_counts()
+    Unique_Learn = df_Learn.drop_duplicates().sort_values(by = 'freq')
+    Total_Unique_Words += len(Unique_Learn)
+    Improve = [x for x in Unique_Learn['word'] if x in false_vocab] 
+    Improve_words = Improve_words+Improve 
+    Improve_point = Improve_point+len(Improve)
     for word in Corlis:
         good_vocab.append([esl,word])
+        learnt_vocab.append(word)
+    for word in Remove:
+        false_vocab.append(word)
     accu = float(len(Corlis))/len(compared_content.split())
-    print accu
+    print "Your accuracy in the ESL {} episode is {:.1f}%".format(esl,accu*100)
+    print "You have learnt {} new unique words".format(len(Unique_Learn))
+    print "The words that you missed and now you can hear are"
+    print " ".join(Improve)
+    print "\n"
+    stat.append([esl,accu,len(Unique_Learn)])
+#    print Unique_Learn
+#    print ",".join(Unique_Learn)
 #    dmef.PRINT(Remove, Insert)
 #    categorise_mistake(Remove,Insert)
-    
-a = pd.DataFrame(good_vocab)
-b= a['word'].value_counts()
+
+print "Summary"
+print "The total unique words that you've learnt is {}".format(Total_Unique_Words)
+print "The total amound of words that you've once missed and now can hear is {}".format(Improve_point)
+print "The words that you now can hear better than before are"
+print " ".join(Improve_words)
+
+df_good_vocab = pd.DataFrame(good_vocab, columns = ['esl','word'])
+#print df_good_vocab['word'].value_counts()
+df_false_vocab = pd.DataFrame(false_vocab, columns = ['word'])
+df_stat = pd.DataFrame(stat,columns = ['esl','accu','unique_learn'])
+#print df_false_vocab['word'].value_counts()
