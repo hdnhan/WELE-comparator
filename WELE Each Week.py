35f20e884fe91587e4d19423cdf7a9aa89ee86b1
hdnhan <hdnhan28@gmail.com>
2019-08-01 02:00:15 +0700

    re-code wele each week

--- WELE Each Week.py
+++ WELE Each Week.py
@@ -3,6 +3,8 @@ from os import listdir
 from os.path import isfile, join
 import Standardize_File_Docx as sfd
 import Detect_Mistakes_Each_File as dmef
+import inflect
+p = inflect.engine()
 
 nesl = input('Number of ESL: ')
 orginial_file_name = '../WELEScripts/ESL ' + nesl + ' WELE.docx'
@@ -12,11 +14,12 @@ path = '../WELE Shared folder/Downloaded Member Scripts/ESL' + nesl
 Remove = []
 Insert = []
 
-def spell(word):
-    for letter in word.split():
-        if len(letter) > 1:
-            return False
-    return True
+def isNumber(word):
+    try:
+        int(word)
+        return True
+    except:
+        return False
 
 for file in listdir(path):
     if isfile(join(path, file)):
@@ -30,74 +33,41 @@ for file in listdir(path):
 
             rem, ins = dmef.Detect_Mistakes_Each_File(orginial_content, compared_content)
             for n in range(len(ins)):
-                if len(rem[n]) != 0 and len(ins[n]) != 0  and \
-                   len(rem[n].split()) < 10 and len(ins[n].split()) < 10 and \
-                   '%09' not in ins[n] and spell(rem[n]) == False: 
-                    Remove.append(rem[n])
-                    Insert.append(ins[n])
+                # At least heard something wrong!
+                if len(rem[n]) != 0 and len(ins[n]) != 0:
+                    # If the fault is too long (more than 10 words) => ignore
+                    if len(rem[n].split()) < 10 and len(ins[n].split()) < 10:
+                        # Convert numbers to words (6 # six, but ignore this wrongdoing)
+                        if (isNumber(rem[n]) or isNumber(ins[n])) and \
+                            (p.number_to_words(rem[n]) == ins[n] or rem[n] == p.number_to_words(ins[n])):
+                            continue
+                        else:
+                            Remove.append(rem[n])
+                            Insert.append(ins[n])
 
-#dmef.PRINT(Remove, Insert)
 
-resrem = []
-resins = []
-stt = []
-check = [False] * len(Remove)
+# Summarize the result
+res = {}
 for n in range(len(Remove)):
-    temp = []
-    if check[n] == False:
-        resrem.append(Remove[n])
-        temp.append(Insert[n])
-    for i in range(n + 1, len(Remove)):
-        if Remove[n] == Remove[i] and check[i] == False:
-            temp.append(Insert[i])
-            check[i] = True
-    
-    
-    if check[n] == False:
-        checktemp = [False] * len(temp)
-        restemp = ''
-        for m in range(len(temp)):
-            cnt = 0
-            if checktemp[m] == False:
-                restemp += str(temp[m])
-                cnt = 1
-            for j in range(m + 1, len(temp)):
-                if temp[m] == temp[j] and checktemp[j] == False:
-                    cnt += 1
-                    checktemp[j] = True
-            if cnt > 0:
-                restemp += ' => ' + str(cnt) + ' | '
-        resins.append(restemp[0:-3])
-        stt.append(len(temp))
-    
-#dmef.PRINT(resrem, resins)
+    res[Remove[n]] = {'sum_err': 0}
 
-dic = []
-for n in range(len(stt)):
-    dic.append((n, stt[n])) 
-def takeSecond(elem):
-    return elem[1]    
-dic.sort(key = takeSecond, reverse = True)    
-stt =  []
-for d in dic:
-    stt.append(d[0])
+for n in range(len(Remove)):
+    res[Remove[n]][Insert[n]] = res[Remove[n]].get(Insert[n], 0) + 1
+    res[Remove[n]]['sum_err'] += 1
 
-RE = []
-IN = []
-for n in stt:
-    RE.append(resrem[n])
-    IN.append(resins[n])
+# Sort 
+res = sorted(res.items(), key = lambda x: x[1]['sum_err'], reverse = True) 
 
-dmef.PRINT(RE, IN)
+for item in res:
+    print('{}: {}' .format(item[0], item[1]))
 
-def WRITECSV(Remove, Insert):
+def WRITECSV(res):
     # create out put file
     f = open('Result Each Week/'+ nesl + '.csv', 'w+')
     f.write('Remove' + ',' + 'Insert' + '\n')
-    for it in range(len(Remove)):
+    for item in res:
         # add new line
-        f.write(Remove[it] + ',' + Insert[it] + '\n')
-    # finish writing file
+        f.write(str(item[0]) + ',' + str(item[1]) + '\n')
     f.close()
 
-WRITECSV(RE, IN)
+WRITECSV(res)
