e58d69d6c0522e005aa11eef86627addd73079b1
hdnhan <hdnhan28@gmail.com>
2019-08-09 00:42:29 +0700

    print in wele each week

--- WELE Each Week.py
+++ WELE Each Week.py
@@ -7,6 +7,10 @@ import inflect
 p = inflect.engine()
 
 nesl = input('Number of ESL: ')
+# create out put file
+# f = open('Result Each Week/'+ nesl + '.csv', 'w+')
+fout = open('Result Each Week/'+ nesl + '.txt', 'w+')
+
 orginial_file_name = '../WELEScripts/ESL ' + nesl + ' WELE.docx'
 orginial_content = sfd.Standardize_File_Docx(orginial_file_name)
 path = '../WELE Shared folder/Downloaded Member Scripts/ESL' + nesl 
@@ -21,13 +25,18 @@ def isNumber(word):
     except:
         return False
 
+totalcnt = 0
+totalerr = 0
 for file in listdir(path):
     if isfile(join(path, file)):
-        if ('(' not in file) and (')' not in file):
+        if ('(' not in file) and (')' not in file) and '.doc' in file:
             #print(file)
             try:
+                totalcnt += 1
                 compared_content = sfd.Standardize_File_Docx(path + '/' + file)
             except Exception as ex:
+                totalerr += 1
+                fout.write(file + '\n')
                 print('{} => {}' .format(file, ex))
                 continue
 
@@ -45,7 +54,7 @@ for file in listdir(path):
                             Remove.append(rem[n])
                             Insert.append(ins[n])
 
-
+fout.write ('\n{} / {}\n\n' .format(totalerr, totalcnt))
 # Summarize the result
 res = {}
 for n in range(len(Remove)):
@@ -55,19 +64,24 @@ for n in range(len(Remove)):
     res[Remove[n]][Insert[n]] = res[Remove[n]].get(Insert[n], 0) + 1
     res[Remove[n]]['sum_err'] += 1
 
-# Sort 
-res = sorted(res.items(), key = lambda x: x[1]['sum_err'], reverse = True) 
+# Sort
+res = dict(sorted(res.items(), key = lambda x: x[1]['sum_err'], reverse = True))
 
-for item in res:
-    print('{}: {}' .format(item[0], item[1]))
+for key, subdict in res.items():
+    print('{} >> {} =>' .format(subdict['sum_err'], key), end = ' ')
+    for k, v in subdict.items():
+        if k is not 'sum_err':
+            print('{}: {}' .format(k, v), end = '   ')
+    print()
 
 def WRITECSV(res):
-    # create out put file
-    f = open('Result Each Week/'+ nesl + '.csv', 'w+')
-    f.write('Remove' + ',' + 'Insert' + '\n')
-    for item in res:
-        # add new line
-        f.write(str(item[0]) + ',' + str(item[1]) + '\n')
-    f.close()
+    fout.write('Error >> Remove => Insert \n')
+    for key, subdict in res.items():
+        fout.write('{} >> {} => ' .format(subdict['sum_err'], key))
+        for k, v in subdict.items():
+            if k is not 'sum_err':
+                fout.write('{}: {}   ' .format(k, v))
+        fout.write('\n')
+    fout.close()
 
 WRITECSV(res)
